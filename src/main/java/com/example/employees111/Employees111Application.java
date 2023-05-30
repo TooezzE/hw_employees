6e707beede87a1131d9fa5cf0bd96bc728dae852
Егор <azazaz.com534@gmail.com>
2023-05-30 18:31:20 +0300

    inintial commit

---
+++ src/main/java/com/example/employees111/Employees111Application.java
@@ -0,0 +1,13 @@
+package com.example.employees111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employees111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Employees111Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employees111/Employees111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employees111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Employees111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
